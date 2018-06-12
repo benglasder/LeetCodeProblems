5a403ebf67cdd450ef881ef0da5dd4bf818d098c
Ben Glasder <bglasder@gmail.com>
2018-06-12 11:11:43 -0500

    Add TwoSum method

--- LeetCodeProblems/Program.cs
+++ LeetCodeProblems/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,8 +11,32 @@ namespace LeetCodeProblems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var nums = new[] {2, 7, 11, 15};
+
+            var solution = TwoSum(nums, 9);
+
+            Console.WriteLine(solution);
+
             Console.ReadKey();
         }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            var hashTable = new Hashtable();
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                int sum = target - nums[i];
+
+                if (hashTable.ContainsKey(sum))
+                {
+                    return new[] {(int) hashTable[sum], i};
+                }
+                hashTable.Add(nums[i], i);
+            }
+
+            throw new Exception("No solution");
+        }
+
     }
 }
