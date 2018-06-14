954a23c46711a912c8fbe887e7e7ed72eeaa73de
Ben Glasder <bglasder@gmail.com>
2018-06-14 10:04:45 -0500

    Add top solution

--- LeetCodeProblems/LeetCodeSolutions.cs
+++ LeetCodeProblems/LeetCodeSolutions.cs
@@ -53,7 +53,11 @@ namespace LeetCodeProblems
             }
             return true;
         }
-
+        /// <summary>
+        ///  My First Attempt
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
         public int MyAtoi(string str)
         {
             // Assume val is positive to begin
@@ -139,6 +143,54 @@ namespace LeetCodeProblems
             Console.WriteLine($"Input: {str} | Output: {result}");
             return isPos ? int.MaxValue : int.MinValue;
 
+        }
+        
+        /// <summary>
+        /// LeetCode top answer
+        /// Initially failed 'OnlyEmptySpaces' Test.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public int MyAtoi2(string str)
+        {
+        
+            int sign = 1; // Will be either 1 or -1
+            int baseX = 0; // our base number
+            int i = 0;
+            if (str.Length <= 0)
+            {
+                Console.WriteLine($"Input: {str} | Output: 0");
+                return 0;
+            }
+            str = str.Trim();
+            // Add check for empty string (All spaces)
+            if (!str.Equals("") && (str[i] == '-' || str[i] == '+'))
+            {
+                // If - exits, subtract 2 from 1 to get -1
+                sign = 1 - 2 * (str[i++] == '-' ? 1 : 0);
+            }
+            // Loop through the array of chars until the end, or we reach a char that isnt numeric
+            while (i < str.Length && str[i] >= '0' && str[i] <= '9')
+            {
+                Console.WriteLine($"{str[i]} | ++ {str[i++]}");
+                if (baseX > Int32.MaxValue / 10 || (baseX == Int32.MaxValue / 10 && str[i] - '0' > 7))
+                {
+                    if (sign == 1)
+                    {
+                        Console.WriteLine($"Input: {str} | Output: {Int32.MaxValue} (Max)");
+                        return Int32.MaxValue;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Input: {str} | Output: {Int32.MinValue} (Min)");
+                        return Int32.MinValue;
+                    }
+                }
+
+                baseX = 10 * baseX + (str[i++] - '0');
+            }
+            Console.WriteLine($"Input: {str} | Output: {baseX * sign}");
+            return baseX * sign;
         }
     }
 }
---
+++ UnitTests/MyAtoi2.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using LeetCodeProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class MyAtoi2
+    {
+        private LeetCodeSolutions leet = new LeetCodeSolutions();
+
+        [TestMethod]
+        public void ReturnZero()
+        {
+
+            var str = "";
+
+            Assert.IsTrue(leet.MyAtoi2(str).Equals(0));
+
+        }
+
+        [TestMethod]
+        public void FourtyTwo()
+        {
+
+            var str = "42";
+
+            Assert.IsFalse(leet.MyAtoi2(str).Equals(0));
+
+            Assert.IsFalse(leet.MyAtoi2(str).Equals(-42));
+
+            Assert.IsTrue(leet.MyAtoi2(str).Equals(42));
+        }
+
+        [TestMethod]
+        public void FourtyTwoTrimSpace()
+        {
+
+            var str = "      -42";
+
+            Assert.IsFalse(leet.MyAtoi2(str).Equals(0));
+
+            Assert.IsFalse(leet.MyAtoi2(str).Equals(42));
+
+            Assert.IsTrue(leet.MyAtoi2(str).Equals(-42));
+        }
+
+        [TestMethod]
+        public void FourOneNineThreeWithWords()
+        {
+
+            var str = "4193 with words";
+
+            Assert.IsFalse(leet.MyAtoi2(str).Equals(0));
+
+            Assert.IsTrue(leet.MyAtoi2(str).Equals(4193));
+        }
+
+        [TestMethod]
+        public void NotValid()
+        {
+
+            var str = "words and 987";
+
+            Assert.IsFalse(leet.MyAtoi2(str).Equals(987));
+
+            Assert.IsTrue(leet.MyAtoi2(str).Equals(0));
+        }
+
+        [TestMethod]
+        public void OutOfRange()
+        {
+
+            var str = "-91283472332";
+
+            Assert.IsFalse(leet.MyAtoi2(str).Equals(0));
+
+            Assert.IsTrue(leet.MyAtoi2(str).Equals(int.MinValue));
+            Assert.IsTrue(leet.MyAtoi2(str).Equals(-2147483648));
+        }
+
+        [TestMethod]
+        public void OnlyEmptySpaces()
+        {
+            var str = " ";
+
+            Assert.IsTrue(leet.MyAtoi2(str).Equals(0));
+
+            str = "         ";
+            Assert.IsTrue(leet.MyAtoi2(str).Equals(0));
+        }
+
+        [TestMethod]
+        public void PlusOrMinusOnly()
+        {
+            var str = "+";
+
+            Assert.IsTrue(leet.MyAtoi2(str).Equals(0));
+
+            str = "-";
+            Assert.IsTrue(leet.MyAtoi2(str).Equals(0));
+        }
+
+        [TestMethod]
+        public void PlusOne()
+        {
+            var str = "+1";
+            Assert.IsTrue(leet.MyAtoi2(str).Equals(1));
+        }
+    }
+}
