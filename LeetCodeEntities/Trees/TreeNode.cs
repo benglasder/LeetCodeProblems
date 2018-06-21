8a6a8442d1d548a13e37c9043ac6e382aa0be6c0
Ben Glasder <bglasder@gmail.com>
2018-06-21 11:05:32 -0500

    Add TreeNode class

--- LeetCodeEntities/Trees/TreeNode.cs
+++ LeetCodeEntities/Trees/TreeNode.cs
@@ -1,4 +1,9 @@
-﻿namespace LeetCodeEntities.Trees
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Configuration;
+
+namespace LeetCodeEntities.Trees
 {
     public class TreeNode
     {
@@ -10,5 +15,81 @@
         {
             val = x;
         }
+
+        public TreeNode(int[] values) : this(values, 0) {}
+
+        private TreeNode(int[] values, int index)
+        {
+            Load(this, values, index);
+        }
+
+        void Load(TreeNode tree, int[] values, int index)
+        {
+            this.val = values[index];
+
+            if (index * 2 + 1 < values.Length)
+            {
+                this.left = new TreeNode(values, index * 2 + 1);
+            }
+
+            if (index * 2 + 2 < values.Length)
+            {
+                this.right = new TreeNode(values, index * 2 + 2);
+            }
+        }
+        
+        public void Print()
+        {
+            Print(this, 1);
+        }
+
+        public void Print(TreeNode p, int padding)
+        {
+            if (p != null)
+            {
+                if (p.right != null)
+                {
+                    Print(p.right, padding + 4);
+                }
+                if (padding > 0)
+                {
+                    Console.Write(" ".PadLeft(padding));
+                }
+                if (p.right != null)
+                {
+                    Console.Write("/\n");
+                    Console.Write(" ".PadLeft(padding));
+                }
+                Console.Write(p.val.ToString() + "\n ");
+                if (p.left != null)
+                {
+                    Console.Write(" ".PadLeft(padding) + "\\\n");
+                    Print(p.left, padding + 4);
+                }
+            }
+        }
+
+        public IList<int> InOrderTraversal()
+        {
+            var list = new List<int>();
+            Stack<TreeNode> nodes = new Stack<TreeNode>();
+            var cur = this;
+            while (nodes.Count != 0 || cur!= null)
+            {
+                while (cur != null)
+                {
+                    nodes.Push(cur);
+                    cur = cur.left;
+                }
+
+                cur = nodes.Pop();
+                list.Add(cur.val);
+                cur = cur.right;
+            }
+
+            return list;
+        }
     }
+    
+    
 }
--- UnitTests/BinarySearchTreeTests.cs
+++ UnitTests/BinarySearchTreeTests.cs
@@ -1,4 +1,6 @@
-﻿using LeetCodeEntities.Trees;
+﻿using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using LeetCodeEntities.Trees;
 using LeetCodeProblems;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -15,6 +17,8 @@ namespace UnitTests
             TreeNode node = new TreeNode(2);
             node.left = new TreeNode(1);
             node.right = new TreeNode(3);
+            
+            node.Print();
             Assert.IsTrue(_leet.IsValidBST(node));
         }
         
@@ -26,7 +30,34 @@ namespace UnitTests
             node.right = new TreeNode(4);
             node.right.left = new TreeNode(3);
             node.right.right = new TreeNode(6);
+            
+            node.Print();
             Assert.IsFalse(_leet.IsValidBST(node));
+        }
+
+        [TestMethod]
+        public void LoadTreeAndValidate()
+        {
+            var values = new int[] {2, 1, 3};
+            var tree = new TreeNode(values);
+            
+            tree.Print();
+            Assert.IsTrue(_leet.IsValidBST(tree));
+        }
+
+        [TestMethod]
+        public void InorderTraversal()
+        {
+            var root = new TreeNode(1);
+            root.right = new TreeNode(2);
+            root.right.left = new TreeNode(3);
+
+            var expected = new int[] {1, 3, 2};
+            var result = root.InOrderTraversal();
+            
+            root.Print();
+            
+            Assert.IsTrue(result.SequenceEqual(expected));
         }
         
     }
