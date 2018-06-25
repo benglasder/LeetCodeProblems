37bde9984f9e51edb5b7b63c9a26cc9713dfa28d
Ben Glasder <bglasder@gmail.com>
2018-06-25 11:58:34 -0500

    added more tests

--- UnitTests/SortingTests.cs
+++ UnitTests/SortingTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text;
 using LeetCodeEntities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -34,10 +35,32 @@ namespace UnitTests
             var result = _sort.BubbleSort(input);
 
             var endTime = DateTime.Now;
+
+
+            var evenNumbers = expected.Where(n => n % 2 == 0).ToList();
             
             Console.WriteLine($"Runtime: {(endTime - startTime).Milliseconds.ToString()}");
             
             Assert.IsTrue(expected.SequenceEqual(result));
         }
+
+        [TestMethod]
+        public void Tests()
+        {
+            var expected = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
+
+            var evenNumbers = expected.Where(n => n % 2 == 0).ToList();
+
+            var str = "ABCDEFG";
+            var sb = new StringBuilder();
+
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                sb.Append(str[i]);
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+       
     }
 }
